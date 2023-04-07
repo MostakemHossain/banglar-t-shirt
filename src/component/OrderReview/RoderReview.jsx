7bff02e5e8d73cfd9baf16f9b8eb02f291734712
MostakemHossain <mostakememon123@gmail.com>
2023-04-07 09:48:48 +0600

    path section added

---
+++ src/component/OrderReview/RoderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RoderReview = () => {
+    return (
+        <div>
+            <h1>This is order Review page</h1>
+        </div>
+    );
+};
+
+export default RoderReview;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './component/Layout/Main';
+import Home from './component/Home/Home';
+import OrderReview from './component/OrderReview/RoderReview';
+
+const router= createBrowserRouter([
+  {
+    path:"/",
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader: ()=> fetch('tshirts.json')
+      },
+      {
+        path:'/review',
+        element:<OrderReview></OrderReview>
+      }
+    ]
+
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
